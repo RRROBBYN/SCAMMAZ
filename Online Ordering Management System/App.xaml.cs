7ed5f40c81cf613ce56aeba66d62281831924283
Robyn Clemente <Robyn Clemente@LAPTOP-5MU13CMT>
2025-09-08 17:45:00 +0800

    Add project files.

---
+++ Online Ordering Management System/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Online_Ordering_Management_System;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
